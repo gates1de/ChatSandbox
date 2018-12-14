19e7e6bf0dcc65ff0362e53cf397bcfc05bad0dc
gates1de <g031i043@gmail.com>
2018-12-14 16:19:50 +0900

    :page_facing_up: Initialize project

---
+++ ChatSandbox/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ChatSandbox
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
